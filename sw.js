e7378c5092f9284552f57e8919999290f3edc9ce
HIMANSHU TIWARI <himutiwari7@gmail.com>
2025-08-07 16:36:49 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  console.log('✅ Service Worker installing...');
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('✅ Service Worker activated
