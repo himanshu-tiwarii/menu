3b1099e2eafe55f93dd49638461be5fa7ab7d26d
HIMANSHU TIWARI <himutiwari7@gmail.com>
2025-08-07 17:07:20 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('✅ Service Worker installing...');
-});
-
-self.addEventListener('activate', (e) => {
-  console.log('✅ Service Worker activated
